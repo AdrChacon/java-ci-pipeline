e32fe010a841976d98cc48f07a5c73a5efdef2c7
Adr-Chaca <36579925+AdrChacon@users.noreply.github.com>
2020-05-20 19:28:40 -0600

    Tests

--- src/calculator/CalculatorTest.java
+++ src/calculator/CalculatorTest.java
@@ -1,7 +1,7 @@
 package calculator;
 
-import org.junit.*;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 public class CalculatorTest {
 	
